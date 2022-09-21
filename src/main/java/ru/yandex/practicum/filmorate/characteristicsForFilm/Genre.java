f25e2a85f860a9c9236d742405d396dc7d73aa6c
HUNTIPUS <zhigulenok18@yandex.ru>
2022-09-21 15:52:32 +0300

    feat: с тестами json все чики бамбони, а вот тесты unit я вообще не поняла как работают, что смогла, написала

---
+++ src/main/java/ru/yandex/practicum/filmorate/characteristicsForFilm/Genre.java
@@ -0,0 +1,2 @@
+package ru.yandex.practicum.filmorate.characteristicsForFilm;public class Genre {
+}
--- src/main/java/ru/yandex/practicum/filmorate/characteristicsUser/Status.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.practicum.filmorate.characteristicsUser;
-
-public enum Status {
-    UNCONFIRMED,
-    CONFIRMED
-}
